9d913f99b788f57af7c80f68d7b57a51662c282e
Lauren Hemingway <lauren_hemingway12@hotmail.com>
2018-09-04 17:52:52 -0400

    Basic HTML, CSS, and minimal JS added

---
+++ main.js
@@ -0,0 +1,27 @@
+https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js
+
+$(document).ready(function() {
+    console.log("Ready")
+
+
+
+let playerHand = 0
+let dealerHand = 0
+
+
+ 
+
+    $( ".deal" ).on( "click", function() {
+        console.log( "haaaay" );
+      });
+
+      $( ".hit" ).on( "click", function() {
+        console.log( "yeah girl" );
+      });
+
+      $( ".stand" ).on( "click", function() {
+        console.log( "omg" );
+      });
+
+});
+
